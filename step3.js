b66efb7f62c1477063d25b0e73fce223976d0092
Jason Smith <jasondsmith2501@gmail.com>
2025-03-09 13:16:52 -0700

    Added step 3 solution

--- step3.js
+++ step3.js
@@ -12,7 +12,7 @@ async function getWebCatOutput(url) {
         }
     } catch (err) {
         console.error(`Error fetching ${url}:`, "\n", "Error: Request failed with status code 404");
-        process.exit();
+        process.exit(1);
     }
 }
 
@@ -22,7 +22,7 @@ async function getCatOutput(path) {
         return data;
     } catch (error) {
         console.error(`Error reading ${path}:`, "\n", error.toString());
-        process.exit();
+        process.exit(1);
     }
 }
 
@@ -43,16 +43,24 @@ async function printReadTarget(targetCat) {
     console.log(output);
 }
 
+async function writeReadTarget(targetCat, writeFile) {
+    const output = await processReadTarget(targetCat);
+
+    try {
+        await fs.writeFile(writeFile, output, "utf-8");
+    } catch (err) {
+        console.error(`Error writing ${writeFile}:`, "\n", error.toString());
+        process.exit(1);
+    }
+}
+
 if (process.argv.length === 3) {
     const targetCat = process.argv[2];
     printReadTarget(targetCat);
 } else if (process.argv.length === 5 && process.argv[2] === "--out") {
-
     const writeFile = process.argv[3];
     const readTarget = process.argv[4];
-
-    console.log("Write a file!", writeFile, readTarget);
-
+    writeReadTarget(readTarget, writeFile);
 } else {
     console.error("Usage: node step3.js <path>");
     console.error("Usage: node step3.js --out <output file> <input file>");
